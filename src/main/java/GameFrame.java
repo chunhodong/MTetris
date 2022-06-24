be2f097d2fcafe4fae3d48eca5bffbe66b84ee63
chunhodong <yhms4432@naver.com>
2022-06-24 22:55:29 +0900

    [add] 화면프레임객체 추가

---
+++ src/main/java/GameFrame.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+
+    //x좌표
+    public final int xPos = 400;
+    //y좌표
+    public final int yPos = 100;
+    //화면크기
+    private Dimension screen=Toolkit.getDefaultToolkit().getScreenSize();
+    //화면가로길이
+    private int width = (int) (screen.width*0.4);
+    //화면세로길이
+    private int height = (int) (screen.height*0.72);
+
+    public GameFrame(){
+
+        setLayout(null);
+        setBounds(xPos,yPos,width,height);
+        setResizable(false);
+        setVisible(true);
+
+    }
+}
