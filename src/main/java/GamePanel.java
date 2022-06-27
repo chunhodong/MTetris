f079b748e905d27e6a2a140a7b9f340a232ae9cc
chunhodong <yhms4432@naver.com>
2022-06-27 23:38:05 +0900

    [fix] 위치초기화

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -1,4 +1,46 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
+
+    /*게임화면 시작X좌표*/
+    public final int xPosition = 10;
+
+    /*게임화면 시작Y좌표*/
+    public final int yPosition = 10;
+    
+    /*게임화면 시작넓이*/
+    public final int width = 250;
+    
+    /*게임화면 높이*/
+    public final int height = 500;
+
+    /**
+     * 게임화면 위치/크기 초기화
+     */
+    public GamePanel(){
+        setLayout(null);
+        setBounds(xPosition,yPosition,width,height);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        paintBackground(g);
+    }
+
+    /**
+     * 그래픽객체가 게임화면을 그림
+     * @param g 그래픽객체
+     */
+    private void paintBackground(Graphics g){
+        for(int i=0;i<20;i++){
+            for(int j=0;j<10;j++){
+                g.setColor(Color.black);
+                g.fillRect(j*25, i*25,25, 25);
+                g.setColor(Color.white);
+                g.drawRect(j*25, i*25,25,25);
+            }
+        }
+    }
 }
