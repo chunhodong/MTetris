35362fb98827d6c50dd0da156cbb8eb42354deec
chunhodong <yhms4432@naver.com>
2022-07-04 23:54:44 +0900

    블록초기화로직 추가

--- src/main/java/GameBlock.java
+++ src/main/java/GameBlock.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.util.Random;
+
 /**
  * 게임블록데이터
  */
@@ -10,11 +13,14 @@ public class GameBlock {
     private int blockType;
     /*블록번호*/
     private int blockNumber;
-
+    /*블록모양타입*/
     private final int blockTypeSize = 7;
-
+    /*블록배열*/
     private int [][] blockArray;
+    
+    private Color[][] blockColorArray;
 
+    private Color blockColor;
 
 
     public void GameBlock(){
@@ -24,9 +30,18 @@ public class GameBlock {
     }
 
     /**
-     * 게임블록모양초기화
+     * 게임블록초기화
      */
     public void initBlock(){
+        initBlockShape();
+        initBlockColor();
+
+    }
+
+    /**
+     * 게임블록모양초기화
+     */
+    private void initBlockShape(){
         blockType = RandomUtils.nextInt(blockTypeSize);
         switch(blockType){
             case 0:
@@ -177,10 +192,21 @@ public class GameBlock {
                 break;
         }
         blockNumber = RandomUtils.nextInt(blockShapeSet.length);
-
     }
 
+    /**
+     * 게임블록색초기화
+     */
+    private void initBlockColor(){
+        Color[] colorArray = new Color[]{Color.BLUE,Color.ORANGE,Color.PINK,Color.RED,Color.ORANGE,Color.YELLOW};
+        blockColor = colorArray[new Random().nextInt(6)];
 
+        
+    }
+
+    /**
+     * 블록배열초기화
+     */
     public void initBlockArray(){
         blockArray = new int[GameOption.BOARD_HEIGHT][GameOption.BOARD_WIDTH];
         int[][] blockShape = blockShapeSet[blockNumber];
@@ -188,10 +214,14 @@ public class GameBlock {
         for(int i = 0; i < blockShape.length; i++){
             for(int j = 0; j < blockShape.length; j++){
                 if(blockShape[i][j] == 1){
-                    blockArray[i][j] = 1;
+                    blockArray[i][j + 4] = 1;
                 }
             }
         }
+    }
+
+    public void rotateBlock(){
+
     }
 
 }
