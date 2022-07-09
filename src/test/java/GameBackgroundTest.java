06dcaa6401ca203fa0b021cd3a656d5498a1afaf
chunhodong <yhms4432@naver.com>
2022-07-09 21:00:41 +0900

    테스트코드추가

---
+++ src/test/java/GameBackgroundTest.java
@@ -0,0 +1,44 @@
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+
+
+public class GameBackgroundTest {
+
+    @DisplayName("게임판널검사")
+    @Test
+    void 게임판널검사(){
+        GameBackground gameBackground = new GameBackground();
+        Assertions.assertThat(gameBackground).extracting("background").isNotNull();
+
+    }
+
+    @Test
+    void 게임판초기화필드값검사(){
+
+        GameBackground gameBackground = new GameBackground();
+        try {
+            Field field = gameBackground.getClass().getDeclaredField("background");
+            field.setAccessible(true);
+
+            int[][] value = (int[][])field.get(gameBackground);
+
+            for(int i = 0; i < value.length; i++){
+                for(int j = 0; j < value[0].length; j++){
+
+                    Assertions.assertThat(value[0][0]).isEqualTo(0);
+
+                }
+            }
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
